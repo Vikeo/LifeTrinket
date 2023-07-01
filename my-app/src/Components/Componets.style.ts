b78321586f3477c303fe7e77a1bce72ccf5ae1a4
Viktor Rådberg <thevikeo@gmail.com>
2023-07-02 00:12:27 +0200

    get basic grid working

---
+++ my-app/src/Components/Componets.style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const CountersWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+`;
+
+export const CountersGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  border-radius: 10px;
+`;
+
+export const GridItem = styled.div`
+  display: flex;
+  width: 50%;
+  height: 50vh;
+  justify-content: center;
+  align-items: center;
+  background-color: antiquewhite;
+  border-radius: 10px;
+`;
---
+++ my-app/src/Components/Counters.tsx
@@ -0,0 +1,25 @@
+import * as S from "./Componets.style";
+
+
+const Counters = () => {
+  return (
+    <S.CountersWrapper>
+        <S.CountersGrid>
+            <S.GridItem>
+                hej1
+            </S.GridItem>
+            <S.GridItem>
+                hej2
+            </S.GridItem>
+            <S.GridItem>
+                hej3
+            </S.GridItem>
+            <S.GridItem>
+                hej4
+            </S.GridItem>
+        </S.CountersGrid>
+    </S.CountersWrapper>
+  );
+}
+
+export default Counters;
