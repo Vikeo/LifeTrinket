df3682a457829ce47a349455b3985aff6dbeb81b
Viktor Rådberg <thevikeo@gmail.com>
2023-08-01 13:29:24 +0200

    adjust layouts

--- my-app/src/Data/getGridTemplateAreas.ts
+++ my-app/src/Data/getGridTemplateAreas.ts
@@ -7,7 +7,7 @@ export enum GridTemplateAreas {
   ThreePlayers = '"player1 player1" "player2 player3"',
   ThreePlayersSide = '"player1 player1 player1  player3" "player2 player2 player2 player3"',
   FourPlayers = '"player1 player2" "player3 player4"',
-  FourPlayersSide = '"player1 player2 player2 player2 player2 player2  player4" "player1 player3 player3 player3 player3 player3 player4"',
+  FourPlayersSide = '"player1 player2 player2 player2 player4" "player1 player3 player3 player3 player4"',
   FivePlayers = '"player1 player1 player1 player2 player2 player2" "player3 player3 player4 player4 player5 player5"',
   FivePlayersSide = '"player1 player1 player1 player1 player1 player2 player2 player2 player2 player2 player3" "player4 player4 player4 player4 player4 player5 player5 player5 player5 player5 player3"',
   SixPlayers = '"player1 player2 player3" "player4 player5 player6"',
