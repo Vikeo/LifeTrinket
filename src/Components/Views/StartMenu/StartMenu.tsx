51e9056eac4a9d54f1be01d66670f4f6f91385ad
Viktor Rådberg <thevikeo@gmail.com>
2023-08-26 18:37:39 +0200

    try enter fullscreen on start

--- src/Components/Views/StartMenu/StartMenu.tsx
+++ src/Components/Views/StartMenu/StartMenu.tsx
@@ -103,6 +103,7 @@ const Start = ({
     if (!initialGameSettings) {
       return;
     }
+    document.documentElement.requestFullscreen();
     setInitialGameSettings(initialGameSettings);
     setPlayers(createInitialPlayers(initialGameSettings));
   };
@@ -113,14 +114,6 @@ const Start = ({
 
   const valuetext = (value: number) => {
     return `${value}`;
-  };
-
-  const toggleFullscreen = () => {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-    } else if (document.exitFullscreen) {
-      document.exitFullscreen();
-    }
   };
 
   const getDefaultLayout = (numberOfPlayers: number) => {
@@ -213,18 +206,6 @@ const Start = ({
             setPlayerOptions({ ...playerOptions, gridAreas })
           }
         />
-        <Button
-          style={{
-            marginTop: '0.5rem',
-            marginRight: 'auto',
-            marginLeft: 'auto',
-          }}
-          size="small"
-          variant="contained"
-          onClick={toggleFullscreen}
-        >
-          Toggle fullscreen
-        </Button>
       </FormControl>
       <StartButtonFooter>
         <Button
