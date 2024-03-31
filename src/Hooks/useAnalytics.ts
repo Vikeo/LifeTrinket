4e6dc56d99d01aa724e848b971d16ee44bacf052
Viktor Rådberg <thevikeo@gmail.com>
2024-03-31 19:02:59 +0200

    add api key back

--- src/Hooks/useAnalytics.ts
+++ src/Hooks/useAnalytics.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics, logEvent } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: 'AIzaSyCZ1AHMb5zmWS4VoRnC-OBxTswUfrJ0mlY',
   authDomain: 'life-trinket.firebaseapp.com',
   projectId: 'life-trinket',
   storageBucket: 'life-trinket.appspot.com',
