824418ed732ddf240f5410b8e96208f879a7f1db
Vikeo <thevikeo@gmail.com>
2025-02-22 11:51:47 +0100

    fix lint

--- src/Components/Misc/TextComponents.tsx
+++ src/Components/Misc/TextComponents.tsx
@@ -2,5 +2,4 @@ import { twc } from 'react-twc';
 
 export const Paragraph = twc.p`text-text-primary`;
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const H1 = twc.h1`text-text-primary;`;
